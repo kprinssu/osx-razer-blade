497f0e03d4e82394a7e14eaccc9cf280027a45c4
Kishor Prins <kprinssu@Kishors-MacBook-Air.local>
2017-04-12 19:15:29 -0700

    Initial Commit

---
+++ RazerBlade/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RazerBlade
+//
+//  Created by Kishor Prins on 2017-04-12.
+//  Copyright © 2017 Kishor Prins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
